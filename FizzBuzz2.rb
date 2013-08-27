1b197c11a9fe053f44c9d399c8abe277fac64e26
Josh Hill <jamesjoshuahill@gmail.com>
2013-08-27 14:22:59 +0100

    Minor change to FizzBuzz2

--- FizzBuzz2.rb
+++ FizzBuzz2.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 
-range_array = (1..100).to_a
-
-range_array.map! do |num|
+range_array = (1..100).to_a.map! do |num|
 	if num % 3 == 0 && num % 5 == 0
 		"FizzBuzz"
 	elsif num % 3 == 0 
